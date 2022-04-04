8cebaf14ff44694049888c9316c72cac39942f6f
blackbox <dzzirima@gmail.com>
2022-04-04 12:47:20 +0200

    feat:able to communicate with blockchain

--- controllers/blockchain.js
+++ controllers/blockchain.js
@@ -1,14 +1,45 @@
+/**This file contains all the methods related in communicating with the blockchain nodes
+ * It can be consumed by any other files just by callin them eg testConnection()
+ */
+
+//importing json
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+const medAcessABI = require("../contracts_abi/prescription.json");
+
 import { ethers } from "ethers";
 
+
 let provider = new ethers.providers.JsonRpcProvider();
+const signer = provider.getSigner(1)
+let abi= medAcessABI.abi;
 
-let getSigner = async () => {
+
+let prescriptionContract = new ethers.Contract("0x0165878A594ca255338adfa4d48449f69242Eb8F",abi,provider)
+
+export const testConnection = async () => {
   // Look up the current block number
-  //const signer = provider.getSigner();
-  let balance = await provider.getBlockNumber();
-  // 14467379
 
-  console.log(balance);
+  try {
+    let balance = await provider.getBlockNumber();
+    if (balance >= 0) {
+      console.log("Blockchain is running ....");
+      console.log("signer :",signer)
+      return true;
+    }
+  } catch (error) {
+    console.log(error.message);
+    return false;
+  }
 };
 
-getSigner();
+export const readBlockchain = async () =>{
+  try {
+    let contractName = await  prescriptionContract.getAccessors()
+    console.log(contractName)
+  } catch (error) {
+    console.log(error.message)
+  }
+}
+
+readBlockchain()
